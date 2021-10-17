3e2b691a90e44946bc428af3b87182a1e9a9d14e
LiudmilaLV <lisikhina.lv@gmail.com>
2021-10-17 17:14:45 +0800

    new database, "add a new project" option

--- website/models.py
+++ website/models.py
@@ -1,16 +1,22 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
-class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project = db.Column(db.String(100))
-    date = db.Column(db.Date, default=func.current_date())
-    duration = db.Column(db.Integer) # Minutes spent on project on particular project session
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False)
+    projects = db.relationship('Project')
+    
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(100), nullable=False)
+    notes = db.Column(db.String(1000), nullable=False, default='')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     entries = db.relationship('Entry')
+    
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default=func.current_date())
+    duration = db.Column(db.Integer, nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
--- website/views.py
+++ website/views.py
@@ -1,12 +1,24 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Project
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/') #methods=['GET', 'POST']
+@views.route('/', methods=['GET', 'POST']) 
 @login_required
 def home():
-    # if request.method == 'POST':
-    #    pass
+    if request.method == 'POST':
+        project_name = request.form.get('project_name')
+        notes = request.form.get('notes')
+        if len(project_name) < 1:
+            flash('Name is too short!', category='warning')
+        elif  Project.query.filter_by(project_name = project_name).first():
+            flash('Project elready exists!', category='warning')
+        else:
+            new_project = Project(project_name = project_name, notes = notes, user_id = current_user.id)
+            db.session.add(new_project)
+            db.session.commit()
+            flash('Project added!', category='success')
         
     return render_template("home.html", user=current_user)
