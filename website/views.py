7d1eb1286f226b8c20c6846cc329c3b9e222becf
LiudmilaLV <lisikhina.lv@gmail.com>
2021-10-28 21:49:57 +0800

    delete project

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, abort, url_for
+from werkzeug.utils import redirect
 from flask_login import login_required, current_user
 from sqlalchemy.orm import selectin_polymorphic
 from flask_sqlalchemy import SQLAlchemy
@@ -27,10 +28,26 @@ def home():
 @login_required
 def project(project_id):
     current_project = Project.query.filter(Project.id==project_id, Project.owner==current_user).first_or_404()
+    last10 = Entry.query.filter(Project.id==project_id, Project.owner==current_user).order_by(Entry.date.desc()).limit(10).all()
     entry_form = EntryForm()       
     if entry_form.validate_on_submit():    
         new_entry = Entry(date=entry_form.date.data, duration=entry_form.duration.data, project_id=current_project.id)
         db.session.add(new_entry)
         db.session.commit()
         flash(f'{entry_form.duration.data} minutes added to the project!', category='success')
-    return render_template("project.html", user=current_user, project_id=current_project.id, name=current_project.project_name, notes=current_project.notes, entry_form=entry_form)
+    return render_template("project.html", user=current_user, project_id=current_project.id, name=current_project.project_name, notes=current_project.notes, entry_form=entry_form, last10=last10)
+
+@views.route('project/<int:project_id>/delete', methods=['POST'])
+@login_required
+def delete_project(project_id):
+    current_project = Project.query.filter(Project.id==project_id, Project.owner==current_user).first_or_404()
+    if current_project.owner != current_user:
+        abort(403)
+    current_project_entries=Entry.query.filter(Entry.project_id==project_id).all()
+    for entry in current_project_entries:
+        db.session.delete(entry)
+    db.session.delete(current_project)
+    db.session.commit()
+    flash(f'Project "{current_project.project_name} has been deleted!', category='success')
+    return redirect(url_for('views.home'))
+    
